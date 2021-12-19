1c4779f28f5841d24f2765504cd8e5fba5820e11
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2021-12-19 15:40:17 -0700

    HTML generated

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs')
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i < profileDataArr.length; i+= 1) {
-    console.log(profileDataArr[i]);
-    }
-    console.log('================');
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
+    
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
