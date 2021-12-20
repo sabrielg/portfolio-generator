37f1510c7cf3051090f23fd3bd5ae47bda823e2d
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2021-12-20 14:52:01 -0700

    template stuff

--- app.js
+++ app.js
@@ -138,7 +138,8 @@ promptUser()
 .then(promptProject)
 .then(portfolioData => {
     console.log(portfolioData);
-      // const pageHTML = generatePage(portfolioData);
+
+      const pageHTML = generatePage(portfolioData);
     // fs.writeFile('./index.html', pageHTML, err => {
     //   if (err) throw new Error(err);
     //   console.log('Page created! Check out index.html in this directory to see it!');
--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,6 @@
-module.exports = (name, github) => {
+module.exports = templateData => {
+    console.log(templateData);
+    
     return `
     <!DOCTYPE html> 
   <html lang="en"> 
