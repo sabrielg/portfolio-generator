9846f87f451fb726aba70604838d998cffb78ad4
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2021-12-21 12:36:04 -0700

    changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs')
 const generatePage = require('./src/page-template.js');
 
+
 // const pageHTML = generatePage(name, github);
 
 
@@ -10,6 +11,7 @@ const generatePage = require('./src/page-template.js');
     
 //     console.log('Portfolio complete! Check out index.html to see the output!');
 // })
+
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -140,10 +142,10 @@ promptUser()
     console.log(portfolioData);
 
       const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
 });
 
 
